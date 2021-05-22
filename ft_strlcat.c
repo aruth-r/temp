406da345a6b9e4684c4eabdea27aec14725c3bc0
aruth-r <alineruthramos@gmail.com>
2021-05-22 16:06:55 -0300

    Teste  strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 14:13:52 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/05/22 15:55:07 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/05/22 16:06:46 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	char *dest;
 	size_t n;
 
-	n = ft_strlen(dst);
+	n = strlen(dst);
 	dest = dst + n;
 	n = dstsize - n -1;
 	ft_memcpy((void*)dest, (void *)src, n);
-	n = ft_strlen(dst);
+	n = strlen(dst);
 	dst[n] = 0;
 	return (n);
 }
