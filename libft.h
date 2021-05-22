a4025126c7f35566d2d61138992a92c7e436157c
aruth-r <alineruthramos@gmail.com>
2021-05-22 16:03:28 -0300

    Teste  strlcat

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 13:01:42 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/05/14 13:15:57 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/05/20 14:37:10 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,19 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
+size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strdup(const char *s);
 
 #endif
